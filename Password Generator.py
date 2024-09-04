c343f8a89ccaf066cb748d9bf7d10081b549b461
aasish0304 <aasishash03@gmail.com>
2024-09-05 00:02:24 +0530

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+
+def generate_password(length):
+    # Define the characters to be used in the password
+    characters=string.ascii_letters + string.digits + string.punctuation
+
+    # Generate a random password
+    password=''.join(random.choice(characters) for _ in range(length))
+    return password
+
+
+def main():
+    # Prompt the user to specify the desired length of the password
+    try:
+        length=int(input("Enter the desired password length: "))
+        if length < 1:
+            print("Password length should be at least 1.")
+            return
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        return
+
+    # Generate and display the password
+    password=generate_password(length)
+    print(f"Generated Password: {password}")
+
+
+if __name__ == "__main__":
+    main()
